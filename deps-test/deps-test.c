b2ad3bdf99900a2495c144d9175deb2c1ce93b9a
rick <rick@ricks-MacBook-Air.local>
2022-07-26 23:29:32 -0400

    integrate uthash

--- deps-test/deps-test.c
+++ deps-test/deps-test.c
@@ -33,6 +33,9 @@
 #include "libtinyfiledialogs/tinyfiledialogs.h"
 #include "libusb/libusb/libusb.h"
 #include "libusb/libusb/os/darwin_usb.h"
+#include "libut/include/libut.h"
+#include "libut/include/ringbuf.h"
+#include "libut/include/utvector.h"
 #include "log.h/log.h"
 #include "miniaudio/miniaudio.h"
 #include "minmax/include/minmax.h"
@@ -40,6 +43,8 @@
 #include "ok-file-formats/ok_png.h"
 #include "ok-file-formats/ok_wav.h"
 #include "tempdir.c/tempdir.h"
+#include "uthash/include/uthash.h"
+#include "uthash/include/uthash.h"
 ////////////////////////////////////////////
 static int do_get_google();
 static inline int file_exists(const char *path);
@@ -2036,6 +2041,114 @@ TEST t_ok_file_format_wav(void){
 }
 
 
+struct my_struct {
+  int            id;           /* key */
+  char           name[21];
+  UT_hash_handle hh;           /* makes this structure hashable */
+};
+
+struct my_struct *users = NULL;
+
+
+void add_user(int user_id, const char *name){
+  struct my_struct *s;
+
+  HASH_FIND_INT(users, &user_id, s);    /* id already in the hash? */
+  if (s == NULL) {
+    s     = (struct my_struct *)malloc(sizeof *s);
+    s->id = user_id;
+    HASH_ADD_INT(users, id, s);      /* id is the key field */
+  }
+  strcpy(s->name, name);
+}
+
+struct my_struct *find_user(int user_id){
+  struct my_struct *s;
+
+  HASH_FIND_INT(users, &user_id, s);    /* s: output pointer */
+  return(s);
+}
+
+
+void delete_user(struct my_struct *user){
+  HASH_DEL(users, user);    /* user: pointer to deletee */
+  free(user);
+}
+
+
+void delete_all(){
+  struct my_struct *current_user;
+  struct my_struct *tmp;
+
+  HASH_ITER(hh, users, current_user, tmp) {
+    HASH_DEL(users, current_user);      /* delete it (users advances to next) */
+    free(current_user);                 /* free it */
+  }
+}
+
+
+void print_users(){
+  struct my_struct *s;
+
+  for (s = users; s != NULL; s = (struct my_struct *)(s->hh.next)) {
+    printf("user id %d: name %s\n", s->id, s->name);
+  }
+}
+
+
+int by_name(const struct my_struct *a, const struct my_struct *b){
+  return(strcmp(a->name, b->name));
+}
+
+
+int by_id(const struct my_struct *a, const struct my_struct *b){
+  return(a->id - b->id);
+}
+
+
+const char *getl(const char *prompt){
+  static char buf[21];
+  char        *p;
+
+  printf("%s? ", prompt); fflush(stdout);
+  p = fgets(buf, sizeof(buf), stdin);
+  if (p == NULL || (p = strchr(buf, '\n')) == NULL) {
+    puts("Invalid input!");
+    exit(EXIT_FAILURE);
+  }
+  *p = '\0';
+  return(buf);
+}
+
+
+TEST t_uthash(void){
+  int              id = 1;
+  struct my_struct *s;
+  int              temp;
+  int              _uid = 12345;
+
+  add_user(id++, "user1");
+  add_user(id++, "user2");
+  add_user(id++, "user3");
+  temp = HASH_COUNT(users);
+  printf("there are %d users\n", temp);
+  print_users();
+
+  s = find_user(1);
+  printf("user: %s\n", s ? s->name : "unknown");
+  printf("  id: %d\n", s ? s->id : -1);
+  if (s != NULL) {
+    delete_user(s);
+  }
+  temp = HASH_COUNT(users);
+  printf("there are %d users\n", temp);
+  print_users();
+
+
+  PASS();
+}
+
+
 TEST t_bitfield(void){
   BENCHMARK(bitfield_benchmark, 1)
   bitfield_t *bf = bitfield_new(100);
@@ -2164,6 +2277,12 @@ SUITE(s_miniaudio) {
   RUN_TESTp(t_miniaudio_play_file, (void *)record_file);
 }
 
+SUITE(s_uthash) {
+  BENCHMARK_QTY(benchmark_uthash, 20)
+  RUN_TEST(t_uthash);
+  END_BENCHMARK(benchmark_uthash)
+  BENCHMARK_SUMMARY(benchmark_uthash);
+}
 SUITE(s_bitfield) {
   RUN_TEST(t_bitfield);
 }
@@ -2271,6 +2390,7 @@ int main(int argc, char **argv) {
   RUN_SUITE(s_minmax);
   RUN_SUITE(s_bench);
   RUN_SUITE(s_bitfield);
+  RUN_SUITE(s_uthash);
   GREATEST_MAIN_END();
   size_t used = do_dmt_summary();
 
